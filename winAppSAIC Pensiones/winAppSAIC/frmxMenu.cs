1d124e91c88b541386bdf6411928c18d437b4107
mariofm2000 <mariofm2000@hotmail.com>
2024-08-25 22:24:41 -0600

    se agrega el formulario frmxMenu.*

--- winAppSAIC Pensiones/winAppSAIC/frmxMenu.cs
+++ winAppSAIC Pensiones/winAppSAIC/frmxMenu.cs
@@ -1334,5 +1334,12 @@ namespace winAppSAIC
             Tesoreria.frmxSaldosTesoreria objSaldosTesoreria = new Tesoreria.frmxSaldosTesoreria();
             objSaldosTesoreria.ShowDialog();
         }
+
+        private void BtnSolPagosProv_ItemClick(object sender, ItemClickEventArgs e)
+        {
+                Catalogos.frmxSolicitudesPagoProveedores objSolPagProveedores = new Catalogos.frmxSolicitudesPagoProveedores();
+                objSolPagProveedores.ShowDialog();
+        }
+
     }
 }
