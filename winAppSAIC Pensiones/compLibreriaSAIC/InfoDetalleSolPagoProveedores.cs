afea628c1e9f1bc0e24718b7a1a3ee1beab6a6b3
mariofm2024 <mario.flores@adnamx.com>
2024-09-12 12:15:02 -0600

    Se agregan InfoSolicitudPagoProveedores e InfoDetalleSolPagoProveedores y archivos a ignorar

---
+++ winAppSAIC Pensiones/compLibreriaSAIC/InfoDetalleSolPagoProveedores.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace compLibreriaSAIC
+{
+    public class InfoDetalleSolPagoProveedores: InfoSolicitudPagoProveedores
+    {
+        public Int32 IdSolicitudDetallePagoProv { get; set; } //IdDetSolTipoPProveedor
+        public Int32 IdTipoPagoProveedor { get; set; }
+        public Int32 IdMatrizTenedora { get; set; }
+        public Int32 IdCliente { get; set; }
+        public Decimal Monto { get; set; }
+        public Int32 IdStatusPago { get; set; }
+        public String IdOperadorPago { get; set; }
+
+    }
+}
